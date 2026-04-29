94004c25dc1803444d8a7ae8617a231b62b0fbd6
Rafael Kedrowski Barocas <rkb6485@moore.wot.ece.northwestern.edu>
2026-04-29 09:47:27 -0500

    set up basic structure for wamr within nautilus

---
+++ src/rt/wamr/platform.c
@@ -0,0 +1,14 @@
+#include "platform_api_vmcore"
+
+// Set up platform resources (none currently)
+int bh_platform_init(void)
+{
+        return 0;
+}
+
+// Destroy platform resources (none currently)
+void bh_platform_destroy(void)
+{
+        return;
+}
+
