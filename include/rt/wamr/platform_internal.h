c92ec03b6c392cf3be8ea4abf8f623d27117f8ce
rkbarocas <rafaelbarocas2025@u.northwestern.edu>
2026-05-13 10:42:41 -0500

    work on correct building for wamr and nk

--- include/rt/wamr/platform_internal.h
+++ include/rt/wamr/platform_internal.h
@@ -1,4 +1,3 @@
-#include "rt/wamr/platform_api_vmcore.h"
 #include "nautilus/thread.h"
 
 typedef korp_tid unsigned long //TODO: not sure if this is safe to do straight up korp_tid is pthread_t is unsigned int, nk uses ulong tids
