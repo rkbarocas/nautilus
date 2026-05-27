d6a0c6aad192449fcbc9af82a97fe598325c665c
rkbarocas <rafaelbarocas2025@u.northwestern.edu>
2026-05-27 11:23:26 -0500

    working through compilation errors

--- include/rt/wamr/platform_internal.h
+++ include/rt/wamr/platform_internal.h
@@ -1,3 +1,40 @@
-#include "nautilus/thread.h"
+#ifndef _PLATFORM_INTERNAL_H
+#define _PLATFORM_INTERNAL_H
 
-typedef korp_tid unsigned long //TODO: not sure if this is safe to do straight up korp_tid is pthread_t is unsigned int, nk uses ulong tids
+#include <nautilus/naut_types.h>
+#include <nautilus/paging.h>
+
+// wamr expects types/functions from these; i'm not sure if nautilus has a pch or something i should use instead of including them individually
+#include <stdint.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+typedef bool_t bool;
+
+typedef void* korp_thread;
+typedef unsigned long korp_tid; //TODO: not sure if this is safe to do straight up korp_tid is pthread_t is unsigned int, nk uses ulong tids
+typedef void* korp_mutex; //TODO
+typedef void* korp_sem;
+
+typedef uint32_t os_file_handle; //TODO: is this right?
+typedef uint32_t os_raw_file_handle;
+
+os_file_handle os_get_invalid_handle(void) //TODO: what does nautilus use? also should this be static inline (some implementations have)
+{
+        return 0;
+}
+
+uint64_t os_getpagesize(void)
+{
+        return nk_paging_default_page_size();
+}
+
+// Extension types (not implemented)
+typedef void* korp_cond;
+typedef void* korp_rwlock;
+typedef void* os_dir_stream;
+typedef void* os_poll_file_handle;
+typedef void* os_nfds_t;
+
+#endif
--- include/rt/wamr/shared_platform.cmake
+++ include/rt/wamr/shared_platform.cmake
@@ -7,8 +7,12 @@ set (NAUTILUS_DIR "~/nautilus")
 include_directories(${PLATFORM_SHARED_DIR})
 include_directories(${PLATFORM_SHARED_DIR}/../include)
 
+include_directories(${NAUTILUS_DIR}/include)
+
 file (GLOB_RECURSE source ${NAUTILUS_DIR}/src/rt/wamr/*.c)
 
 file (GLOB_RECURSE header ${NAUTILUS_DIR}/include/rt/wamr/*.c)
 
 LIST (APPEND RUNTIME_LIB_HEADER_LIST ${header})
+
+MESSAGE ("Completed")
